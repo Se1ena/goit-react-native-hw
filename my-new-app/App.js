3fe3ac40fc95185d971162a84a2fedc06fd7adf1
Veronika Moskaliuk <veronika.moskaliuk.ua@gmail.com>
2023-04-10 11:59:34 +0200

    Hide/Show Keyboard

--- my-new-app/App.js
+++ my-new-app/App.js
@@ -1,33 +1,14 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, Text, ImageBackground } from "react-native";
-// import { RegistrationScreen } from "./Screens/Components/RegistrationScreen/RegistrationScreen";
+//import { RegistrationScreen } from "./Screens/RegistrationScreen/RegistrationScreen";
 import { LoginScreen } from "./Screens/LoginScreen/LoginScreen";
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <ImageBackground
-        style={styles.image}
-        source={require("./assets/images/background-image.jpg")}
-      >
-        {/* <RegistrationScreen /> */}
-        <LoginScreen />
-      </ImageBackground>
+    <>
+      {/* <RegistrationScreen /> */}
+      <LoginScreen />
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    // alignItems: "center",
-    // justifyContent: "center",
-  },
-  image: {
-    flex: 1,
-    resizeMode: "cover",
-    justifyContent: "flex-end",
-  },
-});
--- my-new-app/Screens/LoginScreen/LoginScreen.js
+++ my-new-app/Screens/LoginScreen/LoginScreen.js
@@ -1,30 +1,74 @@
-import { StyleSheet, Text, TextInput, View, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import {
+	StyleSheet,
+	Text,
+	ImageBackground,
+	TextInput,
+	View,
+	TouchableOpacity,
+	Platform,
+	KeyboardAvoidingView,
+	TouchableWithoutFeedback,
+	Keyboard,
+} from "react-native";
 
 export const LoginScreen = () => {
+    const [isShowKeyboard, setIsShowKeyboard] = useState(false);
+
+	const keyboardHide = () => {
+		setIsShowKeyboard(false);
+		Keyboard.dismiss();
+	};
 	return (
-		<View style={styles.box}>
-			<Text style={styles.formTitle}>Войти</Text>
-			<View style={styles.form}>
-				<View style={{ marginBottom: 16 }}>
-					<TextInput style={styles.input} placeholder={"Адрес электронной почты"} placeholderTextColor={"#BDBDBD"} />
-				</View>
-				<View style={{ marginBottom: 43 }}>
-					<TextInput
-						style={styles.input}
-						placeholder={"Пароль"}
-						placeholderTextColor={"#BDBDBD"}
-						secureTextEntry={true}
-					/>
-				</View>
-				<TouchableOpacity activeOpacity={0.8} style={styles.btn}>
-					<Text style={styles.btnTitle}>Войти</Text>
-				</TouchableOpacity>
-			</View>
+		<View style={styles.container}>
+			<TouchableWithoutFeedback onPress={keyboardHide}>
+				<ImageBackground style={styles.image} source={require("../../assets/images/background-image.jpg")}>
+					<KeyboardAvoidingView behavior={Platform.OS == "ios" && "padding"}>
+						<View style={{ ...styles.box, paddingBottom: isShowKeyboard ? 32 : 111 }}>
+							<Text style={styles.formTitle}>Увійти</Text>
+							<View style={styles.form}>
+								<View style={{ marginBottom: 16 }}>
+									<TextInput
+										style={styles.input}
+										placeholder={"Адреса електронної пошти"}
+										placeholderTextColor={"#BDBDBD"}
+										onFocus={() => setIsShowKeyboard(true)}
+									/>
+								</View>
+								<View style={{ marginBottom: 43 }}>
+									<TextInput
+										style={styles.input}
+										placeholder={"Пароль"}
+										placeholderTextColor={"#BDBDBD"}
+										secureTextEntry={true}
+										onFocus={() => setIsShowKeyboard(true)}
+									/>
+								</View>
+								<TouchableOpacity activeOpacity={0.8} style={styles.btnLogin}>
+									<Text style={styles.btnLoginTitle}>Увійти</Text>
+								</TouchableOpacity>
+							</View>
+							<TouchableOpacity style={styles.btnRegister}>
+								<Text style={styles.btnRegisterTitle}>Немає акаунту? Зареєструватися</Text>
+							</TouchableOpacity>
+						</View>
+					</KeyboardAvoidingView>
+				</ImageBackground>
+			</TouchableWithoutFeedback>
 		</View>
 	);
 };
 
 const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+	},
+	image: {
+		flex: 1,
+		resizeMode: "cover",
+		justifyContent: "flex-end",
+	},
 	box: {
 		paddingTop: 32,
 		borderTopRightRadius: 25,
@@ -32,39 +76,53 @@ const styles = StyleSheet.create({
 
 		backgroundColor: "#FFF",
 	},
-	formTitle: { marginBottom: 32, fontSize: 30, textAlign: "center", color: "#212121" },
+	formTitle: {
+		marginBottom: 32,
+		fontSize: 30,
+		textAlign: "center",
+		color: "#212121",
+	},
 	form: {
 		marginHorizontal: 16,
-		backgroundColor: "#FFF",
 	},
 	input: {
 		paddingLeft: 16,
 		paddingRight: 16,
-
 		height: 50,
 		fontSize: 16,
 		lineHeight: 19,
-
 		borderWidth: 1,
 		borderColor: "#E8E8E8",
 		borderRadius: 8,
-
 		backgroundColor: "#F6F6F6",
 		color: "#212121",
 	},
-	btn: {
+	btnLogin: {
+		marginBottom: 16,
 		height: 50,
 
 		justifyContent: "center",
-		alignItems: "center",
-
+        alignItems: "center",
 		borderRadius: 50,
 		backgroundColor: "#FF6C00",
 	},
-	btnTitle: {
+	btnLoginTitle: {
+		fontSize: 16,
+		lineHeight: 19,
+        justifyContent: "center",
+        
+		color: "#FFF",
+	},
+	btnRegister: {
+		marginHorizontal: 50,
+
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	btnRegisterTitle: {
 		fontSize: 16,
 		lineHeight: 19,
 
-		color: "#FFF",
+		color: "#1B4371",
 	},
 });
